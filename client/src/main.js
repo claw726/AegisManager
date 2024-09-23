b80770f0822e35abf8408e5b24c01d800d52368b
Charles Law <calaw726@gmail.com>
2024-09-23 15:53:50 -0400

    Added a login-page

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './routes.js'
 import './styles/tailwind.css';
+import $bus from './utils/events.js'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router);
+
+app.config.globalProperties.$bus = $bus;
+
+app.mount('#app')
