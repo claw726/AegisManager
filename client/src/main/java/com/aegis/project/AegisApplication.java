43247b1df245521e06b4b9d06b2cae1a94eeab29
jayden-deason <ironfist4657@gmail.com>
2024-09-23 16:00:53 -0400

    Added gradle Added SpringBoot Created base AegisApplication Created TestController with /test endpoint

---
+++ client/src/main/java/com/aegis/project/AegisApplication.java
@@ -0,0 +1,11 @@
+package com.aegis.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AegisApplication.class, args);
+    }
+}
---
+++ client/src/main/java/com/aegis/project/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.aegis.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String sayHello() {
+        return "Hello, Aegis!";
+    }
+}
