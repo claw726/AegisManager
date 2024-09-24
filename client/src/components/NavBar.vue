2a06d61bfcbf51f830236181ead5b67f0ee40124
Charles Law <calaw726@gmail.com>
2024-09-24 15:04:54 -0400

    Changed cursor styling

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- Navbar -->
     <div class="relative w-full h-20 bg-secondary flex items-center justify-between px-6">
-      <div @click="goToHome" class="text-white font-bold text-lg">Aegis Manager</div>
+      <div @click="goToHome" class="text-white font-bold text-lg cursor-pointer">Aegis Manager</div>
       <div class="flex space-x-4">
         <button @click="goToLogin"  class="bg-primary border border-highlight text-white font-semibold py-2 px-4 rounded">Log In</button>
         <button @click="goToCreateAcct" class="bg-primary border border-highlight text-white font-semibold py-2 px-4 rounded">Sign Up</button>
