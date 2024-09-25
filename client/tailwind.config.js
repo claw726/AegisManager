606aa20c12cf225da4684dc2fb611a24d6aecd45
Charles Law <calaw726@gmail.com>
2024-09-25 13:04:59 -0400

    Changed styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
     extend: {
       colors: {
         primary: '#202D10',
-        secondary: '#704F32',
-        accent: '#0D203B',
+        secondary: '#0D203B',
+        accent: '#704F32',
         background: '#f3f3f3',
         highlight: '#9DB3C1',
       },
