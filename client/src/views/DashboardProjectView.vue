f1cbaa62902535874dafc5a9ab78d4e602750a88
Charles Law <calaw726@gmail.com>
2024-10-27 20:03:36 -0400

    merge

--- client/src/views/DashboardProjectView.vue
+++ client/src/views/DashboardProjectView.vue
@@ -77,29 +77,6 @@
                 >Created by {{ creator.userName }}</span
               >
             </div>
-
-            <!-- Right Column - Project Info -->
-            <div class="flex-1 space-y-4">
-              <h1 class="text-4xl font-bold text-gray-900">
-                {{ proj.projectName }}
-              </h1>
-              <p class="text-xl text-gray-600">{{ proj.projectDescription }}</p>
-              <div class="flex items-center space-x-2 text-sm text-gray-500">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"
-                  />
-                </svg>
-                <span v-if="creator.userName"
-                  >Created by {{ creator.userName }}</span
-                >
-              </div>
-            </div>
           </div>
         </div>
       </div>
@@ -127,89 +104,74 @@
               ></path>
             </svg>
           </div>
+          <span class="text-lg text-gray-600">Loading Project data...</span>
         </div>
       </div>
+    </div>
 
-      <!-- Search and Create Section -->
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-        <div class="bg-white rounded-xl shadow-md p-6">
-          <div
-            class="flex flex-col md:flex-row items-center justify-between gap-4"
-          >
-            <div class="flex-1 w-full">
-              <div class="relative">
-                <input
-                  type="text"
-                  class="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200"
-                  placeholder="Search projects..."
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5 text-gray-400 absolute left-3 top-2.5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </div>
-            </div>
-            <button
-              @click="goToCreateTask"
-              class="inline-flex items-center px-6 py-3 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition duration-200"
-            >
+    <!-- Search and Create Section -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+      <div class="bg-white rounded-xl shadow-md p-6">
+        <div
+          class="flex flex-col md:flex-row items-center justify-between gap-4"
+        >
+          <div class="flex-1 w-full">
+            <div class="relative">
+              <input
+                type="text"
+                class="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200"
+                placeholder="Search projects..."
+              />
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 mr-2"
+                class="h-5 w-5 text-gray-400 absolute left-3 top-2.5"
                 viewBox="0 0 20 20"
                 fill="currentColor"
               >
                 <path
                   fill-rule="evenodd"
-                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
                   clip-rule="evenodd"
                 />
               </svg>
-              Create New Task
-            </button>
+            </div>
           </div>
+          <button
+            @click="goToCreateTask"
+            class="inline-flex items-center px-6 py-3 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition duration-200"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 mr-2"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            Create New Task
+          </button>
         </div>
       </div>
-      <!-- List of Tasks -->
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div
-          v-if="tasks && tasks.length > 0"
-          class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
-        >
-          <TaskCard
-            v-for="(task, index) in tasks"
-            :key="index"
-            :task="task"
-            :taskIndex="index"
-          />
-        </div>
-        <div v-else class="text-center py-12">
-          <p class="text-gray-500">You have no tasks assigned to you! 🎉.</p>
-        </div>
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          <div
-            v-if="tasks && tasks.length > 0"
-            class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
-          >
-            <TaskCard
-              v-for="(task, index) in tasks"
-              :key="index"
-              :task="task"
-              :taskIndex="index"
-            />
-          </div>
-          <div v-else class="text-center py-12">
-            <p class="text-gray-500">You have no tasks assigned to you! 🎉.</p>
-          </div>
-        </div>
+    </div>
+    <!-- List of Tasks -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div
+        v-if="tasks && tasks.length > 0"
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
+      >
+        <TaskCard
+          v-for="(task, index) in tasks"
+          :key="index"
+          :task="task"
+          :taskIndex="index"
+        />
+      </div>
+      <div v-else class="text-center py-12">
+        <p class="text-gray-500">You have no tasks assigned to you! 🎉.</p>
       </div>
     </div>
   </div>
@@ -221,6 +183,7 @@ import { mapState } from "vuex";
 import TaskCard from "@/components/TaskCard.vue";
 import DropdownMenu from "@/components/DropdownMenu.vue";
 import NotificationComponent from "@/components/NotificationComponent.vue";
+
 export default {
   data() {
     return {
@@ -263,20 +226,7 @@ export default {
   computed: {
     ...mapState("auth", ["isLoggedIn", "currentUser"]),
   },
-
   methods: {
-    goToCreateTask() {
-      // this.$router.push({ name: 'createTask', params: { orgIndex: this.index }});
-      this.$router.push({
-        name: "createTask",
-        params: {
-          orgId: this.$route.params.orgIndex,
-          projId: this.$route.params.projIndex,
-          userID: this.currentUser.userID,
-        },
-      });
-    },
-
     showNotification(type, message, duration = 5000) {
       this.notification = {
         show: true,
@@ -355,7 +305,7 @@ export default {
       // Show confirmation notification
       this.showNotification(
         "warning",
-        "Are you sure you want to delete this project? Click the 'delete' button again to confirm.",
+        "Are you sure you want to delete this project? Click again to confirm.",
         0,
       );
 
