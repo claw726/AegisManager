be390cff68d04123b843d928477cb1c6350ed8cd
Charles Law <calaw726@gmail.com>
2024-10-30 10:50:44 -0400

    small bug fixes

--- client/src/views/ToDoList.vue
+++ client/src/views/ToDoList.vue
@@ -248,7 +248,7 @@ export default {
   },
 
   async mounted() {
-    this.fetchTasks();
+    await this.fetchTasks();
     await this.fetchUniqueAssigners();
   },
 
@@ -271,6 +271,8 @@ export default {
 
     filteredTasks() {
       let tasks = this.tasks;
+
+      
 
       if (this.searchQuery) {
         tasks = tasks.filter((task) =>
